2a75930077b116d5fedcdcfb111ecbbefbbd7f5c
babar016 <babarcse@gmail.com>
2015-06-16 02:29:14 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-    echo 'hudao';
     $kitty = '';
     if(isset ($_GET['greetings'])){
         $kitty = $_GET['greetings'];
-        if(strpos($kitty,"Hello!") >=0 || strpos($kitty,"Hello!") >=0 || strpos($kitty,"Hi!") || >=0 strpos($kitty,"Good morning!") >=0
+        if(strpos($kitty,"Hello!")>=0 || strpos($kitty,"Hello!")>=0 || strpos($kitty,"Hi!")>=0 || strpos($kitty,"Good morning!")>=0
 
-              || strpos($kitty,"Good evening!") >=0 || strpos($kitty,"Good night!") >=0 ){
+              || strpos($kitty,"Good evening!")>=0 || strpos($kitty,"Good night!")>=0 ){
             $a = array('answer'=>'Hello, Kitty! Nice to see you');
             echo json_encode($a);
         }

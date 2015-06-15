3e999c7a7537dcea873621276b3aa5a1644609b8
babar016 <babarcse@gmail.com>
2015-06-16 02:21:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     $kitty = '';
     if(isset ($_GET['greetings'])){
         $kitty = $_GET['greetings'];
-        if(strpos($kitty,"Hello!") || strpos($kitty,"Hello!") || strpos($kitty,"Hi!") || strpos($kitty,"Good morning!")
+        if(strpos($kitty,"Hello!") >0 || strpos($kitty,"Hello!") >0 || strpos($kitty,"Hi!") || >0 strpos($kitty,"Good morning!") >0
 
-              || strpos($kitty,"Good evening!") || strpos($kitty,"Good night!") ){
+              || strpos($kitty,"Good evening!") >0 || strpos($kitty,"Good night!") >0 ){
             $a = array('answer'=>'Hello, Kitty! Nice to see you');
             echo json_encode($a);
         }

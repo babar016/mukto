d4c680731c12cbb0d2cb6e6558316e018c95bad7
babar016 <babarcse@gmail.com>
2015-06-16 02:15:33 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $kitty = '';
+    if(isset ($_GET['greetings'])){
+        $kitty = $_GET['greetings'];
+        if(strpos($kitty,"Hello!") || strpos($kitty,"Hello!") || strpos($kitty,"Hi!") || strpos($kitty,"Good morning!")
+
+              || strpos($kitty,"Good evening!") || strpos($kitty,"Good night!") ){
+            $a = array('answer'=>'Hello, Kitty! Nice to see you');
+            echo json_encode($a);
+        }
+    }
+?>
